7a1af65bc2f5794eb9bf3beb06114a3789d3097b
Himang Dongre <hsdongre2002@gmail.com>
2026-03-01 15:55:00 +0530

    added method: RecursionAndBacktracking/CombinationSum

--- RecursionAndBacktracking/CombinationSum.java
+++ RecursionAndBacktracking/CombinationSum.java
@@ -12,7 +12,8 @@ public class CombinationSum {
     private static List<List<Integer>> combinationSum(int[] candidates, int target){
         List<Integer> current = new ArrayList<>();
         List<List<Integer>> combs = new ArrayList<>();
-        getCombinations(current, target, 0, candidates, combs);
+        // getCombinations(current, target, 0, candidates, combs);
+        getCombinations(target, 0, candidates, current, combs);
         return combs;
     }
 
@@ -38,5 +39,39 @@ public class CombinationSum {
                 remainingSum += candidates[index];
             }
         }
+    }
+
+    // since we need all the paths, will use backtracking
+    // since we need to find combinations, we will use include-exclude pattern
+    // state: remaining sum, remaining candidates
+
+    private static void getCombinations(int remainingSum, int index, int[] candidates, List<Integer> current, List<List<Integer>> combs){
+        // base case
+        if(remainingSum == 0){
+            List<Integer> newList = new ArrayList<>(current);
+            combs.add(newList);
+            return;
+        }
+
+        if(index == candidates.length || remainingSum < 0){
+            return;
+        }
+
+
+        // transition
+        if(remainingSum - candidates[index] >= 0){
+            // action - on path to solution
+            current.add(candidates[index]);
+
+            // recurse
+            getCombinations(remainingSum - candidates[index], index, candidates, current, combs); // include
+
+            // cancellation - backtrack
+            current.remove(current.size() - 1);
+        }
+
+        getCombinations(remainingSum, index + 1, candidates, current, combs); // exclude
+
+
     }
 }
